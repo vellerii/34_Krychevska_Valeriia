2df53ed2648ec4752ac219371be6759c7f9f91f1
Valeriia Krychevska <90756841+vellerii@users.noreply.github.com>
2023-04-05 23:27:12 +0300

    Added Main class

---
+++ Task01/Main.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * The Main class represents the entry point of the program.
+ */
+public class Main {
+
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args The command-line parameters passed to the program. It's an array of String.
+     */
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
